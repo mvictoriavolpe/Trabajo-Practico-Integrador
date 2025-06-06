331931280174aafbe45c4e36e8bbfc89beccfa28
Alx1as <alexiarubin23@gmail.com>
2025-06-06 13:49:07 -0300

    estructura logica del arbol

--- 21.py
+++ 21.py
@@ -25,15 +25,36 @@ HIjo derecho: se planta, no más nodos.
 Podria considerarse que sigue la lógica de preorder creo.
  """
 
-def recorrer_arbol(arbol_juego):
+mano = [] #acá voy a guardar las cartas de los jugadores y acumular sus puntos.
+
+def sacar_carta(): #función para sacar cartas aleatorias
+    carta_random=random.choice(cartas)
+    mano.append(carta_random) 
+    print(f"Te tocó {carta_random}")
+    return(carta_random)
+
+def sumar_mano():
+    return sum(mano)
+
+
+def recorrer_arbol(arbol_juego, primera_vez=True):
     if arbol_juego[1]==[] and arbol_juego[2]==[]: #condición para detectar hojas #además es el caso de corte para la recursión
         print("No hay mas nodos")
+    
     else:
-        print(arbol_juego[0]) #imprime el nodo actual. -->empezar juego
-        empezar=input(f"Tus cartas son: {(random.choice(cartas)),(random.choice(cartas))}. Puntos totales: (aca iria una funcion que suma). \n ¿Quieres pedir otra (1) carta o plantarte (2?")
-        if empezar==1: 
-            arbol_juego[1]
-            print("Te tocó un (carta). Puntaje total:\n Pedir(1) o Plantarse(2)?")
+        if primera_vez:
+            print(arbol_juego[0]) #si es la primera mano imprimo el nodo padre y reparto 2
+            sacar_carta()
+            sacar_carta()
 
+        print(f"Puntos totales: {sumar_mano()}.") #sumo las cartas de la mano.
+        empezar=input(f"¿Quieres pedir otra carta (1)?, o 'Plantarte'(2)?")
+        
+        if empezar == "1": #si el usuario "pide" carta:
+            print(f"Te tocó un {sacar_carta()}. Puntaje total: {sumar_mano()}")
+            recorrer_arbol(arbol_juego[1]) #recorro recursivamente el subárbol izquierdo
 
-recorrer_arbol(arbol_juego)
+        elif empezar == "2":
+            recorrer_arbol(arbol_juego[2]) #recorro el subárbol derecho (plantarse)
+            
+recorrer_arbol(arbol_juego, primera_vez=True)
